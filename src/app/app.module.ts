b8f5d4468d40c414e50638186b73f781b83d3877
AnjasmaraRuin <anjasmaradwisetiadi23@gmail.com>
2022-07-12 09:07:38 +0700

    initialize app navbar, about, actor, movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+/*import * as prime from '../../node_modules/primeflex/primeflex.css';*/
+/*import * as prime from '~/node_modules/primeflex/primeflex.scss/primeflex.css';*/
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MovieComponent } from './movie/movie.component';
+import { ActorComponent } from './actor/actor.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DetailComponent } from './actor/detail/detail.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    ActorComponent,
+    NavbarComponent,
+    DetailComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
